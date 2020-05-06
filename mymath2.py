b5b8767da112bf336e8ae02ef3bbd912724a04db
liang.zhang <liang.zhang@salesforce.com>
2020-05-05 19:34:36 -0700

    add command line support

--- mymath2.py
+++ mymath2.py
@@ -2,6 +2,7 @@
 
 from mathlib import Answer, Score, AdditionQuestion, SubtractionQuestion, MultiplcationQuestion, DivisionQuestion
 from datetime import datetime
+import sys
 
 class Exercise(object):
     def __init__(self, spec, question_type):
@@ -39,46 +40,71 @@ class Exercise(object):
         answer.receive_answer(question.get_question())
         return question.is_answer_correct(answer)
 
+    @classmethod
+    def get_argv_options(cls, argv):
+        add_spec = {
+            'min_first' : 0,
+            'max_first' : 10,
+            'min_second' : 0,
+            'max_second' : 10,
+            'min_final' : 0,
+            'max_final' : 10
+        }
+
+        sub_spec = {
+            'min_first' : 0,
+            'max_first' : 100,
+            'min_second' : 0,
+            'max_second' : 100,
+            'min_final' : 0,
+            'max_final' : 100
+        }
+
+        multi_spec = {
+            'min_first' : 0,
+            'max_first' : 10,
+            'min_second' : 0,
+            'max_second' : 10,
+            'min_final' : 0,
+            'max_final' : 100
+        }
+
+        div_spec = {
+            'min_first' : 1,
+            'max_first' : 100,
+            'min_second' : 1,
+            'max_second' : 10,
+            'min_final' : 0,
+            'max_final' : 10
+        }
+
+        spec = add_spec
+        question_type = AdditionQuestion
+
+        if len(argv) > 1:
+            option = argv[1]
+            print option
+            if option == 'sub':
+                spec = sub_spec
+                question_type = SubtractionQuestion
+            elif option == 'mul':
+                spec = multi_spec
+                question_type = MultiplcationQuestion
+            elif option == 'div':
+                spec = div_spec
+                question_type = DivisionQuestion
+
+        return spec, question_type
+
+
 
 if __name__ == "__main__":
-    add_spec = {
-        'min_first' : 0,
-        'max_first' : 10,
-        'min_second' : 0,
-        'max_second' : 10,
-        'min_final' : 0,
-        'max_final' : 10
-    }
-
-    sub_spec = {
-        'min_first' : 0,
-        'max_first' : 100,
-        'min_second' : 0,
-        'max_second' : 100,
-        'min_final' : 0,
-        'max_final' : 100
-    }
-
-    multi_spec = {
-        'min_first' : 0,
-        'max_first' : 10,
-        'min_second' : 0,
-        'max_second' : 10,
-        'min_final' : 0,
-        'max_final' : 100
-    }
-
-    div_spec = {
-        'min_first' : 1,
-        'max_first' : 100,
-        'min_second' : 1,
-        'max_second' : 10,
-        'min_final' : 0,
-        'max_final' : 10
-    }
-
+    spec, question_type = Exercise.get_argv_options(sys.argv)
+    print spec
+    print question_type
     # es = Exercise(add_spec, AdditionQuestion)
     # es = Exercise(sub_spec, SubtractionQuestion)
     # es = Exercise(multi_spec, MultiplcationQuestion)
-    es = Exercise(div_spec, DivisionQuestion)
+    # es = Exercise(div_spec, DivisionQuestion)
+    es = Exercise(spec, question_type)
     es.run()
